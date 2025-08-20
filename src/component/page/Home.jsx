1b4516e0633c6656464c42a5a330a1c69cdf25eb
fahimhossainmunna <hossainfahim306@gmail.com>
2025-08-21 01:58:18 +0600

    Our Bestsellers

--- src/component/page/Home.jsx
+++ src/component/page/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../layout/Banner'
 import Policy from '../layout/Policy'
 import Ads from '../layout/Ads'
 import NewArrivals from '../layout/NewArrivals'
+import OurBestsellers from '../layout/OurBestsellers'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
    <Policy/>
    <Ads/>
    <NewArrivals/>
+   <OurBestsellers/>
    </>
   )
 }
